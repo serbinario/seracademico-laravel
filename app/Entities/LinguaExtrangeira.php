ff66dccd3ceb7afb144a4ea7ca082e61275c9651
AndreySerbinario <andreyfbs2013@gmail.com>
2016-05-12 16:49:12 -0300

    Atualizações com o crud provisório de vestibulando

--- app/Entities/LinguaExtrangeira.php
+++ app/Entities/LinguaExtrangeira.php
@@ -6,13 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class Taxa extends Model implements Transformable
+class LinguaExtrangeira extends Model implements Transformable
 {
     use TransformableTrait;
 
-    protected $table    = 'taxas';
+    protected $table = "linguas_extrangeiras";
 
-    protected $fillable = [ 
-	];
+    protected $fillable = [
+        'nome'
+
+    ];
 
 }
