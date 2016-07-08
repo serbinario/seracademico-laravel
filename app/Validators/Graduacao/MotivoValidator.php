8c6bbc0bce4e9c3f534e57456f25c46c5500893f
AndreySerbinario <andreyfbs2013@gmail.com>
2016-07-08 09:19:36 -0300

    Atualizações com a implementação da validação de motivos

--- app/Validators/Graduacao/MotivoValidator.php
+++ app/Validators/Graduacao/MotivoValidator.php
@@ -11,8 +11,14 @@ class MotivoValidator extends LaravelValidator
     use TraitReplaceRulesValidator;
 
     protected $rules = [
-        ValidatorInterface::RULE_CREATE => [],
-        ValidatorInterface::RULE_UPDATE => [],
+        ValidatorInterface::RULE_CREATE => [
+            'nome' =>  'required|max:200|unique:fac_motivos,nome',
+            'codigo' =>  'required|max:15|unique:fac_motivos,codigo',
+        ],
+        ValidatorInterface::RULE_UPDATE => [
+            'nome' =>  'required|max:200|unique:fac_motivos,nome,:id',
+            'codigo' =>  'required|max:15|unique:fac_motivos,codigo,:id',
+        ],
    ];
 
 }
