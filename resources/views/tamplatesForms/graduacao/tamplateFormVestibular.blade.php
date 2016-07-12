233c4a1fbd7ffd233bc21ae2a7e48e04f8aafb6e
AndreySerbinario <andreyfbs2013@gmail.com>
2016-07-12 09:11:38 -0300

    Atualizações

--- resources/views/tamplatesForms/graduacao/tamplateFormVestibular.blade.php
+++ resources/views/tamplatesForms/graduacao/tamplateFormVestibular.blade.php
@@ -89,7 +89,7 @@
                         <br/>
                         <div class="row">
 
-                            <div class="col-md-4">
+                            <div class="col-md-3">
                                 <div class="form-group">
                                     @if(isset($model->id))
                                         {!! Form::label('data_ranger', 'Data Inicial - Data Final') !!}
