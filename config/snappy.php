7e6d693c24467b0ddfbd91299a8246db99fe3db9
Paulo Vaz <paulo@serbinario.com.br>
2016-07-29 22:24:22 -0300

    Alteradp config/snappy.php para linux

--- config/snappy.php
+++ config/snappy.php
@@ -4,6 +4,21 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
+        'binary' => '/usr/local/bin/wkhtmltopdf',
+        'timeout' => false,
+        'options' => array(),
+    ),
+    'image' => array(
+        'enabled' => true,
+        'binary' => '/usr/local/bin/wkhtmltoimage',
+        'timeout' => false,
+        'options' => array(),
+    ),
+
+
+
+   /* 'pdf' => array(
+        'enabled' => true,
         'binary' => "\"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe\"",
         'timeout' => false,
         'options' => array(),
@@ -14,6 +29,6 @@ return array(
         'binary' => "\"C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltoimage.exe\"",
         'timeout' => false,
         'options' => array(),
-    ),
+    ),*/
 
 );
