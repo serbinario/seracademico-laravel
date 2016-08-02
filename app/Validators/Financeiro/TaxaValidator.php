4cd8cec73549d1bcad13218bfdae2154facc3800
Felipe Lima <felipeldnascimento@gmail.com>
2016-08-02 09:36:23 -0300

    Alteracoes em TaxaValidator e Create.Taxa

--- app/Validators/Financeiro/TaxaValidator.php
+++ app/Validators/Financeiro/TaxaValidator.php
@@ -13,14 +13,14 @@ class TaxaValidator extends LaravelValidator
     protected $rules = [
         ValidatorInterface::RULE_CREATE => [
             
-            'codigo' =>  'required|unique:fin_taxas,codigo,:id|max:100', //unique
-			'nome' =>  'required|serbinario_alpha_space|max:100',
-            'valor' =>  'required|regex:/^\d{0,6}(\.\d{2})?$/', //$
-            'tipo_taxa_id' =>  'required|integer',
+                'codigo' =>  'required|unique:fin_taxas,codigo,:id|max:100', //unique
+			    'nome' =>  'required|serbinario_alpha_space|max:100',
+                'valor' =>  'required|regex:/^\d{0,6}(\.\d{2})?$/', //$
+                'tipo_taxa_id' =>  'required|integer',
             'valido_inicio' =>  'serbinario_date_format:"d/m/Y"',
             'valido_fim' =>  'serbinario_date_format:"d/m/Y"',
-            'dia_vencimento' =>  'required|integer',
-            'tipo_debito_id' =>  'required|integer',
+                'dia_vencimento' =>  'required|integer',
+                'tipo_debito_id' =>  'required|integer',
             'banco_id' =>  'integer',
             'tipo_multa_id' =>  'integer',
             'valor_multa' =>  'regex:/^\d{0,6}(\.\d{2})?$/' , //$
--- resources/views/tamplatesForms/financeiro/tamplateFormTaxa.blade.php
+++ resources/views/tamplatesForms/financeiro/tamplateFormTaxa.blade.php
@@ -31,14 +31,14 @@
         <div class="row">
             <div class="col-md-1">
                 <div class="form-group">
-                    {!! Form::label('valido_inicio', 'Inicio *') !!}
+                    {!! Form::label('valido_inicio', 'Inicio ') !!}
                     {!! Form::text('valido_inicio', Session::getOldInput('valido_inicio')  , array('class' => 'form-control datepicker')) !!}
                 </div>
             </div>
 
             <div class="col-md-1">
                 <div class="form-group">
-                    {!! Form::label('valido_fim', 'Fim *') !!}
+                    {!! Form::label('valido_fim', 'Fim ') !!}
                     {!! Form::text('valido_fim', Session::getOldInput('valido_fim')  , array('class' => 'form-control datepicker')) !!}
                 </div>
             </div>
@@ -78,7 +78,7 @@
                         <br>
                         <div class="row">
                             <div class="form-group col-md-3">
-                                {!! Form::label('banco_id', 'Banco * ') !!}
+                                {!! Form::label('banco_id', 'Banco ') !!}
                                 {!! Form::select('banco_id', $loadFields['financeiro\\banco'], Session::getOldInput('banco_id'), array('class' => 'form-control')) !!}
                             </div>
                         </div>
@@ -88,24 +88,24 @@
                         <br>
                         <div class="row">
                             <div class="form-group col-md-2">
-                                {!! Form::label('tipo_multa_id', 'Tipo Multa *') !!}
+                                {!! Form::label('tipo_multa_id', 'Tipo Multa ') !!}
                                 {!! Form::select('tipo_multa_id', $loadFields['financeiro\\tipomulta'], Session::getOldInput('tipo_multa_id'), array('class' => 'form-control')) !!}
                             </div>
 
-                            <div class="form-group col-md-1">
-                                {!! Form::label('valor_multa', 'Valor Multa * ') !!}
+                            <div class="form-group col-md-2">
+                                {!! Form::label('valor_multa', 'Valor Multa ') !!}
                                 {!! Form::text('valor_multa', Session::getOldInput('valor_multa'), array('class' => 'form-control moneyReal')) !!}
                             </div>
                         </div>
 
                         <div class="row">
                             <div class="form-group col-md-2">
-                                {!! Form::label('tipo_juro_id', 'Tipo Juros *') !!}
+                                {!! Form::label('tipo_juro_id', 'Tipo Juros ') !!}
                                 {!! Form::select('tipo_juro_id', $loadFields['financeiro\\tipojuro'], Session::getOldInput('tipo_juro_id'), array('class' => 'form-control')) !!}
                             </div>
 
-                            <div class="form-group col-md-1">
-                                {!! Form::label('valor_juros', 'Valor Juros * ') !!}
+                            <div class="form-group col-md-2">
+                                {!! Form::label('valor_juros', 'Valor Juros ') !!}
                                 {!! Form::text('valor_juros', Session::getOldInput('valor_juros'), array('class' => 'form-control moneyReal')) !!}
                             </div>
                         </div>
