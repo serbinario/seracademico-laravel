cb13d5ab2d88f5747ba214532aa91ab2025f6d56
andreyserbinario <andreyfbs2013@gmail.com>
2016-08-05 08:50:14 -0300

    Atualização com a implementação de bugs

--- app/Validators/Graduacao/AlunoValidator.php
+++ app/Validators/Graduacao/AlunoValidator.php
@@ -29,7 +29,7 @@ class AlunoValidator extends LaravelValidator
             'pessoa.nome_pai' => 'required|max:200|serbinario_alpha_space',
             'nome_social' => 'max:200|serbinario_alpha_space',//
             'pessoa.nome_mae' => 'required|max:200|serbinario_alpha_space',
-            //'pessoa.identidade' => 'required|digits_between:4,11|numeric',
+            'pessoa.identidade' => 'required|digits_between:4,11|numeric',
             'orgao_rg' => 'max:30',
             'data_expedicao' => 'serbinario_date_format:"d/m/Y"',
             'pessoa.cpf' => 'required|max:20|unique:pessoas,cpf',
