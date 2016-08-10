06d58c44d6a001a24c5bda5d7a1b7316522b02f6
Paulo Vaz <paulo@serbinario.com.br>
2016-08-10 12:58:18 -0300

    Corrigido MateriaValidator

--- app/Validators/Graduacao/MateriaValidator.php
+++ app/Validators/Graduacao/MateriaValidator.php
@@ -11,25 +11,26 @@ class MateriaValidator extends LaravelValidator
     use TraitReplaceRulesValidator;
 
     protected $messages   = [
+        'anotacao' => 'Anotações'
     ];
 
     protected $attributes = [
-        
+        'anotacao' => 'Anotações'
     ];
 
     protected $rules = [
         ValidatorInterface::RULE_CREATE => [
 
-            'nome' =>  'required|max.string:60|unique:fac_materias,nome',
+            'nome' =>  'required|max:60|unique:fac_materias,nome',
             'codigo' =>  'required|max:8|unique:fac_materias,codigo',
-			'anotacao' =>  'max:500' ,
+			'anotacao' =>  'max:500|serbinario_alpha_space' ,
 
         ],
         ValidatorInterface::RULE_UPDATE => [
 
             'nome' =>  'required|max:60|string|unique:fac_materias,nome,:id',
             'codigo' =>  'required|max:8|unique:fac_materias,codigo,:id',
-			'anotacao' =>  'max:500' ,
+			'anotacao' =>  'max:500|serbinario_alpha_space' ,
 
         ],
    ];
