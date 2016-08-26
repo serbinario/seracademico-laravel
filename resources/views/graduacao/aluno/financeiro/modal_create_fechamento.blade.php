c7ad9371b9038c2864bb57573c5b90b1976a6e52
andreyserbinario <andreyfbs2013@gmail.com>
2016-08-26 12:07:24 -0300

    Atualizações com uma implementação do fehamento

--- resources/views/graduacao/aluno/financeiro/modal_create_fechamento.blade.php
+++ resources/views/graduacao/aluno/financeiro/modal_create_fechamento.blade.php
@@ -10,29 +10,13 @@
             <div class="modal-body" style="alignment-baseline: central">
                 <div class="row">
                     <div class="row">
-                        {{--<div class="form-group col-md-6">--}}
-                            {{--<label for="valor_pago">Valor Pago</label>--}}
-                            {{--<input type="text" name="valor_pago" id="valor_pago" class="form-control">--}}
-                        {{--</div>--}}
-                        <div class="col-md-6">
-                            <label for="valor_total">Valor Total</label>
-                            <input type="text" readonly="readonly" name="valor_total" id="valor_total" class="form-control">
-                        </div>
-
                         <div class="form-group col-md-6">
-                            <label for="valor_debito_fechamento">Valor Débito</label>
-                            <input type="text" disabled="disabled" name="valor_debito_fechamento" id="valor_debito_fechamento" class="form-control">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="form-group col-md-6">
-                            <label for="data_vencimento_fechamento">Vencimento</label>
+                            <label for="data_vencimento_fechamento">Data Vencimento</label>
                             <input type="text" disabled="disabled" name="data_vencimento_fechamento" id="data_vencimento_fechamento" class="form-control datepicker">
                         </div>
 
                         <div class="form-group col-md-6">
-                            <label for="data_fechamento">Data</label>
+                            <label for="data_fechamento">Data Pagamento</label>
                             <input type="text" readonly="readonly" name="data_vencimento_fechamento" value="{{ date('d/m/Y')  }}" id="data_vencimento_fechamento" class="form-control">
                         </div>
                     </div>
@@ -107,6 +91,22 @@
                         <div class="form-group col-md-6">
                             <label for="local_pagamento_id">Local Pagamento</label>
                             <select type="text" name="local_pagamento_id" id="local_pagamento_id" class="form-control"></select>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        {{--<div class="form-group col-md-6">--}}
+                        {{--<label for="valor_pago">Valor Pago</label>--}}
+                        {{--<input type="text" name="valor_pago" id="valor_pago" class="form-control">--}}
+                        {{--</div>--}}
+                        <div class="col-md-6">
+                            <label for="valor_total">Valor Pago</label>
+                            <input type="text" readonly="readonly" name="valor_total" id="valor_total" class="form-control">
+                        </div>
+
+                        <div class="form-group col-md-6">
+                            <label for="valor_debito_fechamento">Valor Débito</label>
+                            <input type="text" disabled="disabled" name="valor_debito_fechamento" id="valor_debito_fechamento" class="form-control">
                         </div>
                     </div>
                 </div>
