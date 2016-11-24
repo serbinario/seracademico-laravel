827f93bdc20f8b4f7896d8ce4e3e95ee8eaf309c
andreyserbinario <andreyfbs2013@gmail.com>
2016-11-24 10:37:38 -0300

    Atualizações

--- resources/views/reports/declaracaoVinculo.blade.php
+++ resources/views/reports/declaracaoVinculo.blade.php
@@ -78,7 +78,7 @@
             font-family: "Arial";
             font-weight: normal;
             text-align: justify;
-            line-height: 1.5;
+            line-height: 1.0;
         }
 
         div#rodape {
