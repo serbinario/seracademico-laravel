1e0172256fe27f628886b038878160abf20d5863
andreyserbinario <andreyfbs2013@gmail.com>
2016-11-24 11:53:05 -0300

    Atualizações

--- resources/views/reports/declaracaoVinculo.blade.php
+++ resources/views/reports/declaracaoVinculo.blade.php
@@ -75,7 +75,7 @@
 
         #main {
             margin: 20px;
-            font-weight: 500;
+            font-weight: 600;
             font-family: "Arial";
             text-align: justify;
             line-height: 1.4;
