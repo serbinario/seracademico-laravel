fed81bd919cda4b5a3df736513bf73937e68c28a
andreyserbinario <andreyfbs2013@gmail.com>
2016-12-09 08:45:16 -0300

    Atualizações

--- resources/views/tamplatesForms/posGraduacao/tamplateFormProfessorPos.blade.php
+++ resources/views/tamplatesForms/posGraduacao/tamplateFormProfessorPos.blade.php
@@ -739,3 +739,295 @@
 </div>
 </div>
 </div>
+
+@section('javascript')
+    <script type="text/javascript">
+
+        Webcam.set({
+            width: 260,
+            height: 240,
+            image_format: 'jpeg',
+            jpeg_quality: 90
+        });
+
+        $(document).on('click', '#foto', function () {
+            Webcam.attach('#my_camera');
+        });
+
+        function take_snapshot() {
+
+            // take snapshot and get image data
+            Webcam.snap(function (data_uri) {
+
+                // display results in page
+                document.getElementById('captura').innerHTML = '<img src="' + data_uri + '"/>';
+                var raw_image_data = data_uri.replace(/^data\:image\/\w+\;base64\,/, '');
+                document.getElementById('cod_img').value = raw_image_data;
+
+                $(".my-profile").modal('hide');
+                Webcam.reset();
+                // $(".modal-dialog").modal('toggle');
+
+            });
+        }
+
+        //Validações javascript
+        $('#formProfessor').bootstrapValidator({
+            fields: {
+                'img': {
+                    validators: {
+                        file: {
+                            maxSize: 819200,   // 2048 * 1024
+                            message: "Tamanho de imagem permitido é de até 800kb"
+                        }
+                    }
+                },
+            },
+        });
+
+        //Carregando as cidades
+        $(document).on('change', "#estado", function () {
+            //Removendo as cidades
+            $('#cidade option').remove();
+
+            //Removendo as Bairros
+            $('#bairro option').remove();
+
+            //Recuperando o estado
+            var estado = $(this).val();
+
+            if (estado !== "") {
+                var dados = {
+                    'table' : 'cidades',
+                    'field_search' : 'estados_id',
+                    'value_search': estado,
+                };
+
+                jQuery.ajax({
+                    type: 'POST',
+                    url: '{{ route('seracademico.util.search')  }}',
+                    data: dados,
+                    datatype: 'json',
+                    headers: {
+                        'X-CSRF-TOKEN' : '{{  csrf_token() }}'
+                    },
+                }).done(function (json) {
+                    var option = "";
+
+                    option += '<option value="">Selecione uma cidade</option>';
+                    for (var i = 0; i < json.length; i++) {
+                        option += '<option value="' + json[i]['id'] + '">' + json[i]['nome'] + '</option>';
+                    }
+
+                    $('#cidade option').remove();
+                    $('#cidade').append(option);
+                });
+            }
+        });
+
+        //Carregando os bairros
+        $(document).on('change', "#cidade", function () {
+            //Removendo as Bairros
+            $('#bairro option').remove();
+
+            //Recuperando a cidade
+            var cidade = $(this).val();
+
+            if (cidade !== "") {
+                var dados = {
+                    'table' : 'bairros',
+                    'field_search' : 'cidades_id',
+                    'value_search': cidade,
+                };
+
+                jQuery.ajax({
+                    type: 'POST',
+                    url: '{{ route('seracademico.util.search')  }}',
+                    headers: {
+                        'X-CSRF-TOKEN': '{{  csrf_token() }}'
+                    },
+                    data: dados,
+                    datatype: 'json'
+                }).done(function (json) {
+                    var option = "";
+
+                    option += '<option value="">Selecione um bairro</option>';
+                    for (var i = 0; i < json.length; i++) {
+                        option += '<option value="' + json[i]['id'] + '">' + json[i]['nome'] + '</option>';
+                    }
+
+                    $('#bairro option').remove();
+                    $('#bairro').append(option);
+                });
+            }
+        });
+
+        //consulta via select2
+        $("#instituicao-graduacao").select2({
+            placeholder: 'Selecione uma instituição',
+            minimumInputLength: 3,
+            width: 400,
+            ajax: {
+                type: 'POST',
+                url: "{{ route('seracademico.util.select2')  }}",
+                dataType: 'json',
+                delay: 250,
+                crossDomain: true,
+                data: function (params) {
+                    return {
+                        'search':     params.term, // search term
+                        'tableName':  'fac_instituicoes',
+                        'fieldName':  'nome',
+                        'fieldWhere':  'nivel',
+                        'valueWhere':  '3',
+                        'page':       params.page || 1
+                    };
+                },
+                headers: {
+                    'X-CSRF-TOKEN' : '{{  csrf_token() }}'
+                },
+                processResults: function (data, params) {
+
+                    // parse the results into the format expected by Select2
+                    // since we are using custom formatting functions we do not need to
+                    // alter the remote JSON data, except to indicate that infinite
+                    // scrolling can be used
+                    params.page = params.page || 1;
+
+                    return {
+                        results: data.data,
+                        pagination: {
+                            more: data.more
+                        }
+                    };
+                }
+            }
+        });
+
+        //consulta via select2
+        $("#instituicao-pos").select2({
+            placeholder: 'Selecione uma instituição',
+            minimumInputLength: 3,
+            width: 400,
+            ajax: {
+                type: 'POST',
+                url: "{{ route('seracademico.util.select2')  }}",
+                dataType: 'json',
+                delay: 250,
+                crossDomain: true,
+                data: function (params) {
+                    return {
+                        'search':     params.term, // search term
+                        'tableName':  'fac_instituicoes',
+                        'fieldName':  'nome',
+                        'fieldWhere':  'nivel',
+                        'valueWhere':  '3',
+                        'page':       params.page || 1
+                    };
+                },
+                headers: {
+                    'X-CSRF-TOKEN' : '{{  csrf_token() }}'
+                },
+                processResults: function (data, params) {
+
+                    // parse the results into the format expected by Select2
+                    // since we are using custom formatting functions we do not need to
+                    // alter the remote JSON data, except to indicate that infinite
+                    // scrolling can be used
+                    params.page = params.page || 1;
+
+                    return {
+                        results: data.data,
+                        pagination: {
+                            more: data.more
+                        }
+                    };
+                }
+            }
+        });
+
+        //consulta via select2
+        $("#instituicao-mestrado").select2({
+            placeholder: 'Selecione uma instituição',
+            minimumInputLength: 3,
+            width: 400,
+            ajax: {
+                type: 'POST',
+                url: "{{ route('seracademico.util.select2')  }}",
+                dataType: 'json',
+                delay: 250,
+                crossDomain: true,
+                data: function (params) {
+                    return {
+                        'search':     params.term, // search term
+                        'tableName':  'fac_instituicoes',
+                        'fieldName':  'nome',
+                        'fieldWhere':  'nivel',
+                        'valueWhere':  '3',
+                        'page':       params.page || 1
+                    };
+                },
+                headers: {
+                    'X-CSRF-TOKEN' : '{{  csrf_token() }}'
+                },
+                processResults: function (data, params) {
+
+                    // parse the results into the format expected by Select2
+                    // since we are using custom formatting functions we do not need to
+                    // alter the remote JSON data, except to indicate that infinite
+                    // scrolling can be used
+                    params.page = params.page || 1;
+
+                    return {
+                        results: data.data,
+                        pagination: {
+                            more: data.more
+                        }
+                    };
+                }
+            }
+        });
+
+        //consulta via select2
+        $("#instituicao-doutorado").select2({
+            placeholder: 'Selecione uma instituição',
+            minimumInputLength: 3,
+            width: 400,
+            ajax: {
+                type: 'POST',
+                url: "{{ route('seracademico.util.select2')  }}",
+                dataType: 'json',
+                delay: 250,
+                crossDomain: true,
+                data: function (params) {
+                    return {
+                        'search':     params.term, // search term
+                        'tableName':  'fac_instituicoes',
+                        'fieldName':  'nome',
+                        'fieldWhere':  'nivel',
+                        'valueWhere':  '3',
+                        'page':       params.page || 1
+                    };
+                },
+                headers: {
+                    'X-CSRF-TOKEN' : '{{  csrf_token() }}'
+                },
+                processResults: function (data, params) {
+
+                    // parse the results into the format expected by Select2
+                    // since we are using custom formatting functions we do not need to
+                    // alter the remote JSON data, except to indicate that infinite
+                    // scrolling can be used
+                    params.page = params.page || 1;
+
+                    return {
+                        results: data.data,
+                        pagination: {
+                            more: data.more
+                        }
+                    };
+                }
+            }
+        });
+    </script>
+@stop
