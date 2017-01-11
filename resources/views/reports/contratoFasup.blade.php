64b1798564615b997bae3150696264673ed0b8bf
= <andreyfbs2013@gmail.com>
2017-01-11 15:58:57 -0300

    Atualizações

--- resources/views/reports/contratoFasup.blade.php
+++ resources/views/reports/contratoFasup.blade.php
@@ -272,7 +272,7 @@
         <b>§ 3°</b> - Em qualquer dos casos dos parágrafos anteriores será procedido o competente processo administrativo.
     </p>
 
-    <div class="footer" style="text-align: center; font-size: 12px;">
+    <div class="footer" style=" margin-top: 5%; text-align: center; font-size: 12px;">
         <p style="margin: 0;">Credenciada pelo MEC, Portaria nº 59 de 19/01/2011, publicada no DOU em 20/01/2011. </p>
         <p style="margin: 0;">Av. Dr. Rodolfo Aureliano, 2182, Vila Torres Galvão – 53430-740 - Paulista-PE.</p>
         <p style="margin: 0;">81-4101-1117 - www.fasup.com</p>
@@ -346,7 +346,7 @@
     </div>
 
 
-    <div class="footer" style="margin-top: 60%; text-align: center; font-size: 12px;">
+    <div class="footer" style="margin-top: 40%; text-align: center; font-size: 12px;">
         <p style="margin: 0;">Credenciada pelo MEC, Portaria nº 59 de 19/01/2011, publicada no DOU em 20/01/2011. </p>
         <p style="margin: 0;">Av. Dr. Rodolfo Aureliano, 2182, Vila Torres Galvão – 53430-740 - Paulista-PE.</p>
         <p style="margin: 0;">81-4101-1117 - www.fasup.com</p>
