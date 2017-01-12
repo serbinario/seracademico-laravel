fabba41682918aee346ac631922058adbfdf9a74
= <andreyfbs2013@gmail.com>
2017-01-12 12:30:47 -0300

    Atualizações

--- resources/views/reports/simple/viewPosAtaFrequencia.blade.php
+++ resources/views/reports/simple/viewPosAtaFrequencia.blade.php
@@ -84,7 +84,7 @@
             var widthPage = document.getElementById("container").offsetHeight;
             var widthBody = document.getElementById("body").offsetHeight;
 
-            document.getElementById("footer").style.marginTop = (widthPage  - widthBody) + 500;
+            document.getElementById("footer").style.marginTop = (widthPage  - widthBody) + 1000;
         }
     </script>
 
