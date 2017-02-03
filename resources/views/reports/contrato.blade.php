0d4671075401ed9d102908ddf6d893a1a399425e
= <andreyfbs2013@gmail.com>
2017-02-03 13:42:40 -0300

    Atualizaçõ

--- resources/views/reports/contrato.blade.php
+++ resources/views/reports/contrato.blade.php
@@ -57,16 +57,14 @@
 
         body {
             font-family: arial;
-            font-size: 14px;
+            font-size: 16px;
         }
 
         table, th, td {
             border: 1px solid black;
             border-collapse: collapse;
         }
-        table , tr , td {
-            font-size: small;
-        }
+
         .termos{
             margin-left: 30px;
             text-align: justify;
@@ -186,7 +184,7 @@ if(isset($turma->aula_inicio) && isset($turma->aula_final)) {
 </table>
 
 <p class="termos">
-    1. <b>O OBJETO</b> – O objeto do presente instrumento é a contratação dos serviços da PRESTADORA DE SERVIÇOS,
+    1. <b>O OBJETO</b> – O objeto do presente instrumento é a contratação dos serviços da PRESTADORA DE SERVIÇOS,&nbsp;&nbsp;&nbsp;
     que se compromete a ministrar aulas dos Cursos de Pós Graduação (lato Sensu), em conformidade com o Curso descrito no quadro 03,
     fornecendo Declaração de Conclusão no final do curso, no prazo máximo de 30 dias,
     ao aluno que obtiver conceito geral mínimo de 7 (sete) pontos por módulo, comparecido em pelo
@@ -208,32 +206,32 @@ $parcelasTxt = Monetary::numberToExt2($turma->qtd_parcelas);
 <p class="termos">
     <?php $vencimento = $turma->vencimento_inicial ? \DateTime::createFromFormat('Y-m-d', $turma->vencimento_inicial) : null; ?>
 
-    2. <b>DO INVESTIMENTO E FORMA DE PAGAMENTO</b> Como contraprestação dos serviços educacionais, acima referidos,
+    2. <b>DO INVESTIMENTO E FORMA DE PAGAMENTO</b>&nbsp;&nbsp;&nbsp; Como contraprestação dos serviços educacionais, acima referidos,
     será cobrado do CONTRATANTE um investimento de R$ {{$valorCurso}} ({{$numeroTxt}}),
     dividido em 1+ {{$turma->qtd_parcelas}} ({{$parcelasTxt}}) parcelas iguais mensais de R$ {{$turma->valor_turma}} com vencimento todo dia {{ isset($vencimento) ? $vencimento->format('d') : 30 }} de cada mês.
     As parcelas podem variar de acordo com cada curso. A matricula só será <b>devolvida</b> caso não forme turma
 </p>
 
 <p class="termos">
-    3. <b>DOS SERVIÇOS NÃO INCLUSOS NO PREÇO DO INVESTIMENTO</b> Não estão incluídos neste Contrato os serviços especiais e as taxas escolares,
+    3. <b>DOS SERVIÇOS NÃO INCLUSOS NO PREÇO DO INVESTIMENTO</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Não estão incluídos neste Contrato os serviços especiais e as taxas escolares,
     assim como: 2º chamada de provas, requerimentos, declarações, certificado e outros serviços.
 </p>
 
 <p class="termos">
-    4. <b>A CARGA HORÁRIA</b>, sua distribuição mensal, o horário das aulas e o prazo de duração do curso encontram-se especificados no quadro 03.
+    4. <b>A CARGA HORÁRIA</b>&nbsp;, sua distribuição mensal, o horário das aulas e o prazo de duração do curso encontram-se especificados no quadro 03.
     A data do termino do Curso poderá ser prorrogada por motivos de força maior ou por
     eventuais ocorrências geradas no decorrer do Curso descrito no campo 03,
     o qual concorda expressamente o aluno, desde que justificado pela Prestadora de Serviços.
 </p>
 
 <p class="termos">
-    5. <b>Fica a critério da PRESTADORA DE SERVIÇO </b> a decisão de suspender o Curso a ser iniciado caso não haja o
+    5. <b>Fica a critério da PRESTADORA DE SERVIÇO </b>&nbsp;&nbsp; a decisão de suspender o Curso a ser iniciado caso não haja o
     preenchimento de 70% (sententa por cento) da capacidade de alunos por turma ou remanejar a data de início escolhida pelo aluno (a),
     mediante acordo entre ambas as partes. Caso o aluno não aceite a mudança o mesmo terá direito a devolução do valor da matrícula.
 </p >
 
 <p class="termos">
-    6. <b>ATRASO(S) DA(S) MENSALIDADE(S) </b> – O Preço dos Serviços ora contratados, bem como o número de mensalidades e data do vencimento
+    6. <b>ATRASO(S) DA(S) MENSALIDADE(S) </b>&nbsp;&nbsp;&nbsp; – O Preço dos Serviços ora contratados, bem como o número de mensalidades e data do vencimento
     ficará especificado na data da Matrícula, ficando ao aluno, o pagamento através das condições escolhidas no ato da
     assinatura do presente contrato. Caso seja escolhida a modalidade do pagamento parcelado, será confeccionado carnê de
     pagamento com a quantidade de mensalidades a serem pagas, o carnê será entregue ao aluno no seu 1º dia de aula,
@@ -242,7 +240,7 @@ $parcelasTxt = Monetary::numberToExt2($turma->qtd_parcelas);
 </p>
 
 <p class="termos">
-    7. <b>VALOR E PAGAMENTO </b> – O atraso ou não pagamento das mensalidades, resultarão em perda do desconto promocional,
+    7. <b>VALOR E PAGAMENTO </b>&nbsp; – O atraso ou não pagamento das mensalidades, resultarão em perda do desconto promocional,
     mais multa de 2% (dois por cento) sobre a importância devida acrescida de 1% (um por cento)
     ao mês mais correção com base no IGPM (FGV) sendo que o recebimento da determinada parcela não significará
     quitação das anteriores. Em caso de mudança na politica financeira nacional convenciona as partes que a
@@ -250,7 +248,7 @@ $parcelasTxt = Monetary::numberToExt2($turma->qtd_parcelas);
 </p>
 
 <p class="termos">
-    8. <b>PERCA DO CARNÊ </b> – O aluno que perde o carnê deverá solicitar segunda via através de requerimento à prestadora de serviço,
+    8. <b>PERCA DO CARNÊ </b>&nbsp; – O aluno que perde o carnê deverá solicitar segunda via através de requerimento à prestadora de serviço,
     mediante pagamento de uma taxa no valor de R$ 15,00.<br />
     - Pagamento realizado através de recibo, será acrescida uma taxa de (R$ 5,00) sobre o valor da mensalidade.<br />
     - Pagamento através de cartão de crédito ou débito o aluno perde o desconto, assim o valor será cheio sem desconto promocional
@@ -258,7 +256,7 @@ $parcelasTxt = Monetary::numberToExt2($turma->qtd_parcelas);
 </p>
 
 <p class="termos">
-    9. <b>TRANCAMENTO </b> – Caso precise, o aluno poderá solicitar através de requerimento o trancamento do seu curso,
+    9. <b>TRANCAMENTO </b>&nbsp; – Caso precise, o aluno poderá solicitar através de requerimento o trancamento do seu curso,
     o mesmo deverá está com as mensalidades em dia e pagar uma taxa no valor de R$ 30,00,
     o trancamento terá a duração de um ano a contar da data da assinatura do requerimento,
     após este prazo o aluno deverá comparecer a prestadora de serviços para formalizar a sua situação,
@@ -268,60 +266,60 @@ $parcelasTxt = Monetary::numberToExt2($turma->qtd_parcelas);
 </p>
 
 <p class="termos">
-    10. <b>DESISTÊNCIA  </b> O aluno poderá desistir do presente contrato no prazo de 7 (sete) dias a contar de sua assinatura e os valores
+    10. <b>DESISTÊNCIA  </b>&nbsp; O aluno poderá desistir do presente contrato no prazo de 7 (sete) dias a contar de sua assinatura e os valores
     eventualmente pagos serão devolvidos. A desistência após este prazo deverá ser solicitada por escrito,
     com 30 (trinta) dias de antecedência, condicionada ao pagamento da multa rescisória equivalente ao valor de 1 (uma)
     mensalidade vincenda sem o <b>DESCONTO PROMOCIONAL</b> , sendo assim, valor da <b>MENSALIDADE CHEIA</b>, e deverá estar em dia com os pagamentos contratados.
 </p>
 
 <p>
-    <b>Parágrafo Único:</b> Caso o pedido de desistência previsto nesta cláusula não seja formalizado,
+    <b>Parágrafo Único:</b>&nbsp; Caso o pedido de desistência previsto nesta cláusula não seja formalizado,
     o contrato continuará em vigor e a Contratante deverá pagar todas as parcelas previstas no ato da inscrição,
     podendo a primeira parte Contratada tomar medidas cabíveis de cobrança.
 </p>
 
 <p class="termos">
-    11. <b>INADIMPLÊNCIA  </b> – O aluno desde já, autoriza a PRESTADORA DE SERVIÇOS em caso de Inadimplência,
+    11. <b>INADIMPLÊNCIA  </b>&nbsp; – O aluno desde já, autoriza a PRESTADORA DE SERVIÇOS em caso de Inadimplência,
     a emitir a Duplicata de Prestação de Serviços com o vencimento a vista e enviar ao Cartório de Protesto,
     bem como proceder ao registro do débito junto ao serviço de Proteção ao Crédito – SPC e Serasa,
     ficando a critério da PRESTADORA DE SERVIÇOS, rescindir o presente contrato nos casos de Inadimplência superior a 60 (sessenta) dias
 </p>
 
 <p class="termos">
-    12. Sempre que a <b>Contratante</b> mudar de endereço deverá comunicar de imediato por escrito as <b>Contratadas</b>,
+    12. Sempre que a <b>Contratante</b>&nbsp; mudar de endereço deverá comunicar de imediato por escrito as <b>Contratadas</b>,
     sob pena de terem-se como válidas e eficazes todas as correspondências enviadas pelas <b>Contratadas</b> para endereço anterior,
     constante deste contrato.
 </p>
 
 <p>
-    <b>Parágrafo Primeiro:</b> A <b>Contratante</b> deverá possuir um endereço eletrônico permanente para contato com as <b>Contratadas</b> e
+    <b>Parágrafo Primeiro:</b>&nbsp; A <b>Contratante</b> deverá possuir um endereço eletrônico permanente para contato com as <b>Contratadas</b> e
     com o (s) professor (s). É de total responsabilidade da <b>Contratante</b> adquirir e informar um endereço eletrônico as <b>Contratadas</b>,
     não importando os meios pelos quais irá adquiri-lo.
 </p>
 
 <p>
-    <b>Parágrafo Segundo:</b> A <b>Contratante</b> autoriza a divulgação de sua imagem nas propagandas da instituição.
+    <b>Parágrafo Segundo:</b>&nbsp; A <b>Contratante</b> autoriza a divulgação de sua imagem nas propagandas da instituição.
 </p>
 
 <p class="termos">
-    13. <b>DANOS </b> – O aluno que danificar bens móveis, equipamentos ou mesmo o prédio  da PRESTADORA DE SERVIÇO,
+    13. <b>DANOS </b>&nbsp; – O aluno que danificar bens móveis, equipamentos ou mesmo o prédio  da PRESTADORA DE SERVIÇO,&nbsp;&nbsp;
     deverá ressarcir em dinheiro o dano ou repor os bens danificados por outros equivalentes ou de mesma qualidade
     e uma vez apurado por ele causado e não havendo ressarcimento ou reposição do bem danificado, fica autorizado a
     PRESTADORA DE SERVIÇOS, a qualquer tempo, a propositura das ações cabíveis para a satisfação de seu prejuízo e a rescisão contratual.
 </p>
 
 <p class="termos">
-    14. <b>RESCISÃO CONTRATUAL </b> – Constitui em causa para a rescisão contratual, além do constante na cláusula 9,
+    14. <b>RESCISÃO CONTRATUAL </b>&nbsp; – Constitui em causa para a rescisão contratual, além do constante na cláusula 9,
     o descumprimento de qualquer cláusula contratual, sendo ainda rescindido o contrato uma vez provado por declarações de terceiros,
     atos de indisciplina do aluno.
 </p>
 
 <p class="termos">
-    15. <b>TOLERÂNCIA </b> – A tolerância da PRESTADORA DE SERVIÇOS ou do aluno não significará renúncia, perdão, renovação ou alteração do que foi contratado.
+    15. <b>TOLERÂNCIA </b>&nbsp; – A tolerância da PRESTADORA DE SERVIÇOS ou do aluno não significará renúncia, perdão, renovação ou alteração do que foi contratado.
 </p>
 
 <p class="termos">
-    16. <b>FORO </b> – As partes elegem o foro desta comarca como o único competente para dirimir qualquer questão oriunda do presente contrato,
+    16. <b>FORO </b>&nbsp; – As partes elegem o foro desta comarca como o único competente para dirimir qualquer questão oriunda do presente contrato,
     em detrimento de qualquer outro, por mais privilegiado que possa ser.<br />
     E por estarem justas e contratadas, as partes firmam o presente contrato em 02 (duas) vias de igual teor,
     subscritos pelas testemunhas abaixo nomeadas, declarando ainda o aluno/responsável legal haver lido e
