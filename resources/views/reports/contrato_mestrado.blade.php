36be47541edf88fdda435481cbb8c282af567c6e
andreypriscila <andreyfbs2013@gmail.com>
2017-02-20 14:51:19 -0300

    Atualizações

--- resources/views/reports/contrato_mestrado.blade.php
+++ resources/views/reports/contrato_mestrado.blade.php
@@ -416,18 +416,18 @@
         igual teor e forma, para que produza seus jurídicos e legais efeitos.
     </p>
 
-   <p style="text-align: center; margin-top: 10%; margin-bottom: 10%;">
+   <p style="text-align: center; margin-top: 50%; margin-bottom: 10%;">
        ______________/_______,_______de______________________________de_____________________.
    </p>
 
-    <div style="margin-top: 50%; margin-left: 0;">
+    <div style="margin-left: 0;">
         <h1 style="text-align: left;">
             <img src="{{ asset('/img/assinatura_contrato_mestrado.png') }}" alt="">
         </h1>
     </div>
 
     <div>
-        <h1 style="margin-top: 70%; text-align: center">
+        <h1 style="margin-top: 80%; text-align: center">
             <img src="{{ asset('/img/rodape_contrato_mestrado.png') }}" alt="">
         </h1>
     </div>
