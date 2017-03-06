2ee5b05f38bea5764266cc7d43d3ef8f1d08515d
andreypriscila <andreyfbs2013@gmail.com>
2017-03-06 09:40:36 -0300

    Atualizações

--- resources/views/reports/contrato_mestrado.blade.php
+++ resources/views/reports/contrato_mestrado.blade.php
@@ -445,7 +445,7 @@
     </div>
 
     <div>
-        <h1 style="text-align: center; margin-top: 115%;">
+        <h1 style="text-align: center; margin-top: 110%;">
             <img src="{{ asset('/img/rodape_contrato_mestrado.png') }}" alt="">
         </h1>
     </div>
