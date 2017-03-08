9052cf0e37b6220b809f15bc61c63fa77f174650
andreypriscila <andreyfbs2013@gmail.com>
2017-03-08 07:45:08 -0300

    Atualizações

--- resources/views/reports/contrato_mestrado.blade.php
+++ resources/views/reports/contrato_mestrado.blade.php
@@ -6,7 +6,7 @@
     <style type="text/css">
         body {
             font-family: Arial, Helvetica, AppleGothic, sans-serif;
-            font-size: 15px;
+            font-size: 12px;
         }
         .cabecalho {
             width: 80%;
@@ -107,7 +107,7 @@
     </p>
 
     <div class="informacoes_pessoas">
-        <table class="table" style="font-size: 15px;">
+        <table class="table" style="font-size: 12px;">
             <tr>
                 <td colspan="4">Nome: {{$aluno['pessoa']['nome']}}</td>
             </tr>
