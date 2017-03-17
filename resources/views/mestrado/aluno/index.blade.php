bdf6984578814f6c985966117515c8c82f04cec8
andreypriscila <andreyfbs2013@gmail.com>
2017-03-17 12:24:42 -0300

    Atualizações

--- resources/views/mestrado/aluno/index.blade.php
+++ resources/views/mestrado/aluno/index.blade.php
@@ -116,6 +116,32 @@
                     </div>
                 </div>
             </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="headingOne">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                        Relatórios Avançados
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                                <div class="panel-body">
+                                    <div class="row">
+                                        <div class="form-group col-md-4">
+                                            {!! Form::select('relatorios', ( ['' => 'Selecione um relatório'] + $loadFields['simplereport']->toArray()),
+                                                Session::getOldInput('relatorios'), array('class' => 'form-control', 'id' => 'report_id')) !!}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -129,6 +155,8 @@
     @include('mestrado.aluno.curriculo.modal_editar_dispensar_disciplina')
     @include('mestrado.aluno.curriculo.modal_create_disciplina_extra_curricular')
     @include('mestrado.aluno.curriculo.modal_create_equivalencia')
+    @include('reports.simple.modals.modal_report_mes_aluno_geral')
+    @include('reports.simple.modals.modal_report_mes_aluno_documento')
 @stop
 
 @section('javascript')
@@ -140,6 +168,8 @@
     <script type="text/javascript" src="{{ asset('/js/mestrado/aluno/modal_editar_dispensar_disciplina.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/js/mestrado/aluno/curriculo/modal_create_disciplina_extra_curricular.js') }}"></script>
     <script type="text/javascript" src="{{ asset('/js/mestrado/aluno/curriculo/modal_create_equivalencia.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/report/simple/modal_report_mes_aluno_geral.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/report/simple/modal_report_mes_aluno_documento.js') }}"></script>
 
     {{--Fabio--}}
     <script type="text/javascript" src="{{ asset('/js/mestrado/aluno/documentos/modal_aluno_documento.js') }}"></script>
@@ -228,5 +258,36 @@
 
             $("#modal-aluno-documento").modal({show:true});
         });
+
+        // Geriamento dos relatórios avançadas
+        $(document).on('change', '#report_id', function () {
+            // Recuperando o id do relatório
+            var reportId = $('#report_id').val();
+
+            // Validando o id do relatório
+            if(!reportId) {
+                return false;
+            }
+
+            // Fazendo a requisição ajax
+            jQuery.ajax({
+                type: 'GET',
+                url: '/index.php/seracademico/report/getFunction/' + reportId,
+                datatype: 'json'
+            }).done(function (retorno) {
+                // Verificando o retorno da requisição
+                if(retorno.success) {
+                    execute(new Function(retorno.dados.function));
+                } else {
+                    // Retorno tenha dado erro
+                    swal(retorno.msg, "Click no botão abaixo!", "error");
+                }
+            });
+        });
+
+        // Função utilizada para executar o callback
+        function execute(callback) {
+            callback();
+        }
     </script>
 @stop
