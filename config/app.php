3548781d01066575b173162b114e667bf30ac923
andreypriscila <andreyfbs2013@gmail.com>
2017-03-27 09:21:12 -0300

    Atualização

--- config/app.php
+++ config/app.php
@@ -140,10 +140,10 @@ return [
         /*
         * Gerador de PDF
         */
-       // Barryvdh\Snappy\ServiceProvider::class,
+        Barryvdh\Snappy\ServiceProvider::class,
 
         //Form
-       //Illuminate\Html\HtmlServiceProvider::class,
+        //Illuminate\Html\HtmlServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
 
         /*
@@ -156,7 +156,7 @@ return [
 
         Prettus\Repository\Providers\RepositoryServiceProvider::class,
         \Seracademico\Providers\SeracademicoRepositoryProvider::class,
-        //Barryvdh\DomPDF\ServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
         /*
         * Laravel Framework Service Providers...
