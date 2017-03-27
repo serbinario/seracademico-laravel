b5f39e12e8ab9bb4090ab48ccb4df15547af51c6
andreypriscila <andreyfbs2013@gmail.com>
2017-03-27 08:52:48 -0300

    Atualização

--- app/Services/PosGraduacao/AlunoService.php
+++ app/Services/PosGraduacao/AlunoService.php
@@ -95,7 +95,7 @@ class AlunoService
             'pessoa.instituicaoEscolar',
             'curriculos'
         ];
-        
+
         $aluno = $this->repository->with($relacionamentos)->find($id);
 
         #Verificando se o registro foi encontrado
