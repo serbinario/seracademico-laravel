34a792d7659559ffc618e3fb26d9562a8f1a1f69
andreypriscila <andreyfbs2013@gmail.com>
2017-04-28 07:42:02 -0300

    Atualizações

--- resources/views/reports/simple/viewPosCadernetaFrequencia.blade.php
+++ resources/views/reports/simple/viewPosCadernetaFrequencia.blade.php
@@ -4,12 +4,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <title></title>
     <style type="text/css">
+        body {
+            font-family: Arial, Helvetica, AppleGothic, sans-serif;
+        }
+
         #logoTopo {
             margin-bottom: 20px;
         }
 
         #tituloDocumento {
-            background-color: #c8c8c8;
+            background-color: #DCDCDC;
             font-family: Arial;
             font-size: 16px;
             text-align: center;
@@ -17,11 +21,12 @@
         }
 
         table {
+            font-size: 12px;
             margin-top: 0;
         }
 
         #subTitulo {
-            background-color: #c8c8c8;
+            background-color: #DCDCDC;
             font-family: Arial;
             font-size: 15px;
             margin-bottom: 20px;
@@ -32,8 +37,12 @@
             margin: 0;
         }
 
+        td {
+            padding: 0.8%;
+        }
+
         thead th {
-            background-color: #c8c8c8;
+            background-color: #DCDCDC;
             text-align: center;
         }
 
@@ -48,6 +57,7 @@
         }
 
         .textCenter {
+            width: 4%;
             text-align: center;
 
         }
@@ -60,30 +70,34 @@
     <h1>
         <img id="logoTopo" src="{{ asset('img/logo_modelo.png') }}" alt="Logo faculdade modelo">
     </h1>
+
     <h1 id="tituloDocumento">
         DIÁRIO DE CLASSE
     </h1>
-    <table id="tabelaSupeior" width="544" cellspacing="0">
+
+    <table id="tabelaSupeior" style="margin-bottom: 2%" width="544" cellspacing="0">
         <tbody>
         <tr>
-            <td>Curso: </td>
-            <td>Turma: </td>
+            <td><b>Curso:</b> {{ $dados["cursoTurmaDisciplinaProfessor"][0]->nomeCurso ?? "" }}</td>
+            <td><b>Turma:</b> {{ $dados["cursoTurmaDisciplinaProfessor"][0]->codigoTurma ?? "" }}</td>
         </tr>
         <tr>
-            <td colspan="2">Disciplina: </td>
+            <td colspan="2"><b>Disciplina:</b> {{ $dados["cursoTurmaDisciplinaProfessor"][0]->nomeDisciplina ?? "" }}</td>
         </tr>
         <tr>
-            <td colspan="2">Carga Horária: </td>
+            <td colspan="2"><b>Carga Horária:</b> {{ $dados["cursoTurmaDisciplinaProfessor"][0]->cargaHoraria ?? "" }}</td>
         </tr>
         <tr>
-            <td>Professor Responsável: </td>
-            <td>Titulação: </td>
+            <td><b>Professor Responsável:</b> {{ $dados["cursoTurmaDisciplinaProfessor"][0]->nomeProfessor ?? "" }}</td>
+            <td><b>Titulação:</b> {{ $dados["cursoTurmaDisciplinaProfessor"][0]->nomeTitulacao ?? "" }}</td>
         </tr>
         </tbody>
     </table>
-    <h1 id="subTitulo" class="textCenter">
+
+    <h1 id="subTitulo" style="text-align: center;">
         ALUNOS EFETIVOS
     </h1>
+
     {{--Dados da turma--}}
 </div>
 <div id="body">
@@ -93,16 +107,24 @@
         <tr>
             <th rowspan="3">N°</th>
             <th rowspan="3">Nome do(a) Aluno(a)</th>
-            <th colspan="4">Dias Letivos</th>
+            <th colspan="11">Dias Letivos</th>
             <th rowspan="3">Percentual de Frequência (%)</th>
             <th colspan="3">Notas</th>
         </tr>
         <tr>
             <th class="textCenter">Mês</th>
             {{--colunas verticais abaixo de dias letivos--}}
-            <td class="textCenter">1</td>
-            <td class="textCenter">2</td>
-            <td class="textCenter">3</td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+
             {{--colunas abaixo da coluna notas--}}
             <th rowspan="2">Prova</th>
             <th rowspan="2">Trab.</th>
@@ -110,59 +132,90 @@
         </tr>
         <tr>
             <th class="textCenter">Dias</th>
-            <td class="textCenter">7</td>
-            <td class="textCenter">8</td>
-            <td class="textCenter">9</td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
+            <td class="textCenter"></td>
         </tr>
         </thead>
         <tbody>
-        <tr>
-            <td class="textCenter">1</td>
-            <td>Francisco</td>
-            <td></td>
-            <td class="textCenter">F</td>
-            <td class="textCenter">P</td>
-            <td class="textCenter">F</td>
-            <td class="textCenter">50%</td>
-            <td class="textCenter">7,0</td>
-            <td class="textCenter">7,0</td>
-            <td class="textCenter">7,0</td>
-        </tr>
+
+        <?php $cout = 0; ?>
+
+        @foreach($dados["alunos"] as $aluno)
+            <tr>
+                <td class="textCenter">{{ ++$cout }}</td>
+                <td style="font-size: 10px; padding-top: 0;">{{ $aluno->nome }}</td>
+                <td></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+                <td class="textCenter"></td>
+            </tr>
+        @endforeach
+
         </tbody>
     </table>
     {{--Dados dos alunos--}}
     {{--Diario de aula--}}
-    <h1 id="tituloDocumento">
+    <h1 id="tituloDocumento" style="margin-bottom: 3%; page-break-before: always;">
         RESUMO DO CONTEÚDO MINISTRADO
     </h1>
-    <table>
+
+    <table class="tabelaDiario" style="width: 100%;" cellspacing="0">
         <thead>
             <tr>
-                <th rowspan="6">1ª Aula</th>
-                <th>Data: ...... / ...... /........</th>
-                <th>Horas</th>
+                <th rowspan="11">1ª Aula</th>
+                <th style="text-align: left">Data: ...... / ...... /........</th>
+                <th style="text-align: left" >Horas: </th>
             </tr>
         </thead>
         <tbody>
         <tr>
-            <td>1</td>
-            <td>2</td>
+            <td colspan="2" style="height: 2%"></td>
         </tr>
         <tr>
-            <td>3</td>
-            <td>4</td>
+            <td colspan="2" style="height: 2%"></td>
         </tr>
         <tr>
-            <td>5</td>
-            <td>6</td>
+            <td colspan="2" style="height: 2%"></td>
         </tr>
         <tr>
-            <td>7</td>
-            <td>8</td>
+            <td colspan="2" style="height: 2%"></td>
         </tr>
         <tr>
-            <td>9</td>
-            <td>10</td>
+            <td colspan="2" style="height: 2%"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 2%"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 2%"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 2%"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 2%"></td>
+        </tr>
+        <tr>
+            <td colspan="2" style="height: 2%"></td>
         </tr>
         </tbody>
     </table>
