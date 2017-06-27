1ce3ec4fbbfe267c5c09ffec8c78ae088a514bb8
Felipe <felipeldnascimento@gmail.com>
2017-06-27 09:44:52 -0300

    Atualizacoes

--- resources/views/reports/declaracaoVinculoModelo.blade.php
+++ resources/views/reports/declaracaoVinculoModelo.blade.php
@@ -1,4 +1,5 @@
 <?php
+
 // leitura das datas automaticamente
 
 function data($dia, $mes, $ano, $semana) {
@@ -60,7 +61,7 @@ function data($dia, $mes, $ano, $semana) {
             border-width: thick;
         }
 
-        #background {
+        /*#background {
             width: 100%;
             height: 100%;
             background-image: url("{{ asset('/img/marca_dagua_modelo.png') }}");
@@ -69,7 +70,7 @@ function data($dia, $mes, $ano, $semana) {
             background-position: center;
             background-size: 500px 700px;
             position: absolute;
-        }
+        }*/
         @media print
         {
             body {font-family:georgia, times, serif;}
@@ -94,85 +95,79 @@ function data($dia, $mes, $ano, $semana) {
         table , tr , td {
             font-size: small;
         }
+
+        #logo{
+            width: 70px;
+            height: auto;
+            margin-left: 293px;
+        }
     </style>
 </head>
 
 <body>
-    <div id="background">
-    </div>
+<div id="background">
+</div>
 
-    <div id="container">
-        <div id="main">
-            <div style="margin-right: 500px;">
-                <img src="{{ asset('img/logo_modelo.png')  }}" alt="">
-            </div>
-            <center><h4 style="margin-top:10px; margin-bottom: 5px;">FACULDADE MODELO - FACIMOD</h4></center>
+<div id="container">
+    <div id="main">
+        <div style="margin-right: 500px;">
+            <img src="{{ asset('img/logo_modelo.png')  }}" alt="" id="logo">
+        </div>
+        <br/>
+        <center><div style="font-size: 10px; margin-top:10px; margin-bottom: 5px;">
+                Credenciada pela Portaria Ministerial n° 2.413 de 11/08/2004 (D.O.U. de 12/08/2004)</br>
+                Mantida pelo Instituto Modelo de Ensino Superior Ltda.</br>
+                CNPJ: 05.121.388/0001-00</br>
+                Centro de Estudos Avançados de Extensão e Pós Graduação – CEA</div></center>
+        <br/>
+        <center>
+            <span><b>DECLARAÇÃO DE VÍNCULO</b></span>
+        </center>
+        <br/>
+        <div style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+            O Centro de Estudos Avançados de Extensão e Pós Graduação da Faculdade Modelo – CEA/PE por competência delegada e amparado:
+        </div>
+        <div style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+            a) Nota Técnica n° 388/2013 – CGLNRS/DPR/SERES/MEC, de 21 de junho de 2013.
+        </div>
+        <div style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+            b) Resolução CNE/CES n° 01/2007, n° 04/2011 e n° 07/2011, por dispositivo da Portaria Normativa MEC n° 40/2007, republicada em 29/12/2010.
+        </div>
+        <div style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+            c) Parecer CNE/CES n° 263/2006.
+        </div>
 
-            <center><h4 style="margin-top: 5px; margin-bottom: 20px;">Credenciada pela Portaria Ministerial nº 2.413</h4></center>
+        <p style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+            Declara que para os devidos fins que <b>{!! isset($aluno['pessoa']) ? $aluno['pessoa']['nome'] : "" !!}</b>,
+            matricula de Nº <b>{!! isset($aluno['matricula']) ? $aluno['matricula'] : "" !!}</b>,
+            portadora do CPF: <b>{!! isset($aluno->pessoa['cpf']) ? $aluno->pessoa['cpf'] : "" !!}</b> e
+            RG: <b>{!! isset($aluno->pessoa['identidade']) ? $aluno->pessoa['identidade'] : "" !!}</b>
+            <b>{!! isset($aluno->pessoa['orgao_rg']) ? $aluno->pessoa['orgao_rg'] : "" !!}</b>,
+            está matriculada (o) no Curso de Especialização de
+            Pós Graduação LATO SENSU em Educação Inclusiva e Coordenação Pedagógica, ministrado por esta
+            Instituição o qual está devidamente registrado no Sistema e-MEC.
+            Com carga horária total de 420 horas/aulas, iniciado em 04 de março de 2017 com previsão de término no
+            dia, 05 de Maio de 2018. Aulas aos sábados, das 08h às 17h.
+        </p>
 
-            <center>
-                <span><b>DECLARAÇÃO DE VÍNCULO</b></span>
-            </center>
+        <p style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+            O Referido é verdadeiro e dou fé.
+        </p>
 
-            <br />
+        <center><p style="font-size: 14px;text-indent: 2em; margin-top: 10px;">
+                Luciana Teixeira Vitor<br>
+                Gestora do Centro de Estudos Avançados<br>
+                De Extensão Pós Graduação<br>
+                CEA/PE
+            </p></center>
 
-            <p style="font-size: 20px;text-indent: 2em; margin-top: 10px;">
-                Declaramos, para os devidos fins de direito, que <b>{!! isset($aluno['pessoa']) ? $aluno['pessoa']['nome'] : "" !!}</b>,
-                matrícula nº <b>{!! isset($aluno['matricula']) ? $aluno['matricula'] : "" !!}</b> está regulamente matriculado (a)
-                nesta Instituição de Ensino Superior, no curso de Pós-graduação “Lato Sensu”,
-                em <b>{!! isset($curso->nome) ? $curso->nome : "" !!}</b>, realizado e certificado pela Faculdade Modelo - FACIMOD
-                de acordo com a portaria de credenciamento do MEC de n° 2.413 de 11/08/2004.
-            </p>
-
-            <p style="font-size: 20px;text-indent: 2em;">
-                <?php
-                    if(isset($turma->aula_inicio) && isset($turma->aula_final)) {
-                        $aulaIni = \DateTime::createFromFormat('Y-m-d', $turma->aula_inicio);
-                        $aulaFim = \DateTime::createFromFormat('Y-m-d', $turma->aula_final);
-                    } else {
-                        $dataFromat = "";
-                    }
-                ?>
-                O curso de Pós-Graduação atendeu ao disposto na Resolução CES/CNE-
-                MEC Nº 01 de 2007, com carga horária total de {!! isset($curso->carga_horaria) ? $curso->carga_horaria : "" !!}horas/aula, iniciado no dia
-                <?php data($aulaIni->format('d'), $aulaIni->format('m'), $aulaIni->format('Y'), $aulaIni->format('w')); ?> com previsão de término no dia
-                <?php data($aulaFim->format('d'), $aulaFim->format('m'), $aulaFim->format('Y'), $aulaFim->format('w')); ?>.
-                Aulas aos sábados, das 08:00 às 17:00 horas.
-            </p>
-
-            <div id="rodape">
-                <p style="text-indent: 2em;">
-                    O referido é verdadeiro e dou fé.
-                </p>
-                <br />
-
-                <div style="position: absolute; margin-top: -20px; margin-left: 30%;">
-                    <img width="200px" height="200px" src="{{ asset('img/assinatura_luciana.png')  }}" alt="">
-                </div>
-
-                <center>
-                    <span>
-                        Recife, <?php $data = new \DateTime('now'); data($data->format('d'), $data->format('m'), $data->format('Y'), $data->format('w')); ?>
-                    </span>
-                </center>
-
-                <center>
-                    <p style="margin-top: 60px;">
-                        Secretária Acadêmica
-                    </p>
-                </center>
-
-                <div id="rodape" style="margin-top: 50px;">
-                    <div style="text-align: center; font-size: 11px;">
-                        <p style="margin: 0;"><b>Faculdade Modelo - FACIMOD.</b></p>
-                        <p style="margin: 0;">Credenciada pela Portaria Ministerial n° 2.413 de 11/08/2004 (D.O.U. de 12/08/2004).</p>
-                        <p style="margin: 0;">Mantida pelo  Instituto Modelo de Ensino Superior Ltda.</p>
-                        <p style="margin: 0;">CNPJ: 05.121.388/0001-00</p>
-                    </div>
-                </div>
+        <div id="rodape" style="margin-top: 50px;">
+            <div style="text-align: center; font-size: 11px;">
+                <p style="margin: 0;">Rua Engenheiro Benedito Mario da Silva nº 95, Bairro Cajuru</p>
+                <p style="margin: 0;">Curitiba – Paraná Te 041 32 26 454 5 – http://www.facimod.com.br/</p>
+                <p style="margin: 0;">CEA/PE: Rua Gervásio Pires, 826 – Santo Amaro – Recife/PE</p>
             </div>
         </div>
     </div>
-
 </body>
 </html>
