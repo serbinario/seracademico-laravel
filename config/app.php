fa3c4fc62b9d8e73feaffa7d54e1fb8110fcc47d
root <root@ip-10-0-2-105.us-east-2.compute.internal>
2017-07-06 13:39:04 +0000

    Atualizações

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
 
     'key' => env('APP_KEY', 'SomeRandomString'),
 
-    'cipher' => MCRYPT_RIJNDAEL_128,//'AES-256-CBC',
+    'cipher' => 'AES-256-CBC',
 
     /*
     |--------------------------------------------------------------------------
