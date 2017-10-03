703dc435e3166ee7a2fe81062a73c3d4e3678cb8
Felipe <felipeldnascimento@gmail.com>
2017-10-03 11:00:33 -0300

    Atualizacoes

--- resources/views/reports/contrato_doutorado.blade.php
+++ resources/views/reports/contrato_doutorado.blade.php
@@ -458,7 +458,7 @@
     </div>
 
     <div>
-        <h1 style="text-align: center; margin-top: 15%">
+        <h1 style="text-align: center; margin-top: 30%">
             <img src="{{ asset('/img/rodape_contrato_mestrado.png') }}" alt="">
         </h1>
     </div>
