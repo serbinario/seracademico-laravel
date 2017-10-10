d029f8154d6e65cbcd93c2c812ab66ecf5cf8243
Andrey <root@ip-10-0-2-105.us-east-2.compute.internal>
2017-10-10 12:33:05 -0300

    Atualizações

---
+++ public/js/posgraduacao/aluno/financeiro/modal_debitos.js
@@ -0,0 +1,79 @@
+// Função para carregar a grid
+var tableDebitos;
+function loadTableDebitos (idAluno) {
+    tableDebitos = $('#grid-debitos').DataTable({
+        processing: true,
+        serverSide: true,
+        retrieve: true,
+        iDisplayLength: 5,
+        bLengthChange: false,
+        bFilter: false,
+        autoWidth: false,
+        ajax: "/index.php/seracademico/posgraduacao/aluno/financeiro/gridDebitos/" + idAluno,
+        columns: [
+            /*{
+                "className":      'details-control',
+                "orderable":      false,
+                "data":           null,
+                "defaultContent": ''
+            },*/
+            {data: 'nomeTaxa', name: 'nomeTaxa'},
+            {data: 'data_vencimento', name: 'data_vencimento'},
+            {data: 'valor_debito', name: 'valor_debito'},
+            {data: 'situacaoBoleto', name: 'situacaoBoleto'},
+            {data: 'pago', name: 'pago'},
+            {data: 'action', name: 'action', orderable: false, searchable: false}
+        ]
+    });
+
+    // array de detalhes da grid
+    var detailRows = [];
+
+    // evento para criação dos detalhes da grid
+    $('#grid-acursar').on( 'click', 'tr td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = tableACursar.row( tr );
+        var idx = $.inArray( tr.attr('id'), detailRows );
+
+        if ( row.child.isShown() ) {
+            tr.removeClass( 'details' );
+            row.child.hide();
+
+            // Remove from the 'open' array
+            detailRows.splice( idx, 1 );
+        }
+        else {
+            tr.addClass( 'details' );
+            row.child( formatAcursar( row.data() ) ).show();
+
+            // Add to the 'open' array
+            if ( idx === -1 ) {
+                detailRows.push( tr.attr('id') );
+            }
+        }
+    } );
+
+    // On each draw, loop over the `detailRows` array and show any child rows
+    tableDebitos.on( 'draw', function () {
+        $.each( detailRows, function ( i, id ) {
+            $('#'+id+' td.details-control').trigger( 'click' );
+        } );
+    } );
+
+    return tableDebitos;
+}
+
+// Função para executar a grid
+function runFinanceiro(idAluno) {
+    // Carregando a grid de debitos
+    if(tableDebitos) {
+        loadTableDebitos(idAluno).ajax
+            .url("/index.php/seracademico/posgraduacao/aluno/financeiro/gridDebitos/" + idAluno)
+            .load();
+    } else {
+        loadTableDebitos(idAluno);
+    }
+
+    // carregando a modal
+    $("#modal-debitos").modal({show:true});
+}
---
+++ public/js/posgraduacao/aluno/financeiro/modal_info_debito.js
@@ -0,0 +1,36 @@
+$(document).on("click", "#btnInfoDebito", function () {
+    var idDebito = tableDebitos.row($(this).parents('tr')).data().id;
+
+    jQuery.ajax({
+        type: 'GET',
+        url: '/index.php/seracademico/posgraduacao/aluno/financeiro/infoDebito/' + idDebito,
+        datatype: 'json',
+
+    }).done(function (retorno) {
+        if(retorno.success) {
+            loadInfoDebitoDom(retorno.debito);
+            $('#modal-informacacoes-debito').modal({show: true});
+        } else {
+            swal(retorno.msg, "Click no botão abaixo!", "error");
+        }
+    });
+});
+
+function loadInfoDebitoDom(debito) {
+    $('#infoCodTaxa').html(debito.taxa.codigo);
+    $('#infoTipoTaxa').html(debito.taxa.tipo_taxa.nome);
+    $('#infoValorTaxa').html(debito.taxa.valor);
+    $('#infoVencimentoDebito').html(debito.data_vencimento);
+    $('#infoValorDebito').html(debito.valor_debito);
+    $('#infoFormPagDebito').html(debito.forma_pagamento ? debito.forma_pagamento.nome : '');
+    $('#infoSituacaoDebito').html(debito.pago ? "Sim" : "Não");
+
+    $('#infoBoletoCodigo').html(debito.boleto ? debito.boleto.gnet_charge : '');
+    $('#infoValorBoleto').html(debito.boleto ? debito.boleto.gnet_valor : '');
+    $('#infoSituacaoBoleto').html(debito.boleto ? debito.boleto.status_gnet.nome : '');
+    $('#infoLinkBoleto').html(
+        debito.boleto
+            ? '<a target="_blank" href="'+ debito.boleto.gnet_link +'">Abrir o boleto em outra página</a>'
+            : ''
+    );
+}
