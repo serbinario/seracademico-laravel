c2c258fb8e8f7a9c8ef8901ef132d826bed4a4a2
andreypriscila <andreyfbs2013@gmail.com>
2017-10-18 12:33:03 -0300

    Atualizações

--- app/Services/Graduacao/VestibulandoService.php
+++ app/Services/Graduacao/VestibulandoService.php
@@ -7,6 +7,7 @@ use Seracademico\Entities\Graduacao\Vestibulando;
 use Seracademico\Entities\Graduacao\VestibulandoFinanceiro;
 use Seracademico\Entities\Graduacao\VestibulandoNotaVestibular;
 use Seracademico\Repositories\EnderecoRepository;
+use Seracademico\Repositories\Financeiro\ContaBancariaRepository;
 use Seracademico\Repositories\Graduacao\AlunoRepository;
 use Seracademico\Repositories\Graduacao\VestibulandoFinanceiroRepository;
 use Seracademico\Repositories\Graduacao\VestibulandoNotaVestibularRepository;
@@ -15,9 +16,13 @@ use Seracademico\Repositories\Graduacao\VestibularRepository;
 use Seracademico\Repositories\PessoaRepository;
 use Seracademico\Repositories\Graduacao\VestibulandoDocumentoRepository;
 use Seracademico\Facades\ParametroMatriculaFacade;
+use Seracademico\Services\Financeiro\DebitoService;
+use Seracademico\Services\TraitService;
 
 class VestibulandoService
 {
+    use TraitService;
+
     /**
      * @var VestibulandoRepository
      */
@@ -51,11 +56,6 @@ class VestibulandoService
     /**
      * @var VestibulandoFinanceiroRepository
      */
-    private $financeiroRepository;
-
-    /**
-     * @var VestibulandoFinanceiroRepository
-     */
     private $vestibulandoDocumentoRepository;
 
     /**
@@ -67,6 +67,15 @@ class VestibulandoService
      * @var AlunoService
      */
     private $alunoService;
+    /**
+     * @var DebitoService
+     */
+    private $debitoService;
+
+    /**
+     * @var ContaBancariaRepository
+     */
+    private $contaBancariaRepository;
 
     /**
      * Método Construtor
@@ -81,7 +90,6 @@ class VestibulandoService
      * @param VestibularRepository $vestibularRepository
      * @param VestibulandoNotaVestibularRepository $notaRepository
      * @param AlunoRepository $alunoRepository
-     * @param VestibulandoFinanceiroRepository $financeiroRepository
      * @param AlunoService $alunoService
      */
     public function __construct(
@@ -91,19 +99,21 @@ class VestibulandoService
         VestibularRepository $vestibularRepository,
         VestibulandoNotaVestibularRepository $notaRepository,
         AlunoRepository $alunoRepository,
-        VestibulandoFinanceiroRepository $financeiroRepository,
         AlunoService $alunoService,
-        VestibulandoDocumentoRepository $vestibulandoDocumentoRepository)
+        DebitoService $debitoService,
+        VestibulandoDocumentoRepository $vestibulandoDocumentoRepository,
+        ContaBancariaRepository $contaBancariaRepository)
     {
-        $this->repository           = $repository;
-        $this->pessoaRepository     = $pessoaRepository;
-        $this->enderecoRepository   = $enderecoRepository;
+        $this->repository = $repository;
+        $this->pessoaRepository = $pessoaRepository;
+        $this->enderecoRepository = $enderecoRepository;
         $this->vestibularRepository = $vestibularRepository;
-        $this->notaRepository       = $notaRepository;
-        $this->alunoRepository      = $alunoRepository;
-        $this->financeiroRepository = $financeiroRepository;
-        $this->alunoService         = $alunoService;
-        $this->vestibulandoDocumentoRepository  = $vestibulandoDocumentoRepository;
+        $this->notaRepository = $notaRepository;
+        $this->alunoRepository = $alunoRepository;
+        $this->alunoService = $alunoService;
+        $this->debitoService = $debitoService;
+        $this->vestibulandoDocumentoRepository = $vestibulandoDocumentoRepository;
+        $this->contaBancariaRepository = $contaBancariaRepository;
     }
 
     /**
@@ -382,151 +392,6 @@ class VestibulandoService
     }
 
     /**
-     * Método load
-     *
-     * Método responsável por recuperar todos os models (com seus repectivos
-     * métodos personalizados para consulta, se for o caso) do array passado
-     * por parâmetro.
-     *
-     * @param array $models || Melhorar esse código
-     * @return array
-     */
-    public function load(array $models, $ajax = false) : array
-    {
-        #Declarando variáveis de uso
-        $result    = [];
-        $expressao = [];
-
-        #Criando e executando as consultas
-        foreach ($models as $model) {
-            # separando as strings
-            $explode   = explode("|", $model);
-
-            # verificando a condição
-            if(count($explode) > 1) {
-                $model     = $explode[0];
-                $expressao = explode(",", $explode[1]);
-            }
-
-            #qualificando o namespace
-            $nameModel = "\\Seracademico\\Entities\\$model";
-
-            #Verificando se existe sobrescrita do nome do model
-            //$model     = isset($expressao[2]) ? $expressao[2] : $model;
-
-            if ($ajax) {
-                if(count($expressao) > 0) {
-                    switch (count($expressao)) {
-                        case 1 :
-                            #Recuperando o registro e armazenando no array
-                            $result[strtolower($model)] = $nameModel::{$expressao[0]}()->orderBy('nome', 'asc')->get(['nome', 'id', 'codigo']);
-                            break;
-                        case 2 :
-                            #Recuperando o registro e armazenando no array
-                            $result[strtolower($model)] = $nameModel::{$expressao[0]}($expressao[1])->orderBy('nome', 'asc')->get(['nome', 'id', 'codigo']);
-                            break;
-                        case 3 :
-                            #Recuperando o registro e armazenando no array
-                            $result[strtolower($model)] = $nameModel::{$expressao[0]}($expressao[1], $expressao[2])->orderBy('nome', 'asc')->get(['nome', 'id', 'codigo']);
-                            break;
-                    }
-
-                } else {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::orderBy('nome', 'asc')->get(['nome', 'id']);
-                }
-            } else {
-                if(count($expressao) > 1) {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::{$expressao[0]}($expressao[1])->lists('nome', 'id');
-                } else {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::lists('nome', 'id');
-                }
-            }
-
-            # Limpando a expressão
-            $expressao = [];
-        }
-
-        #retorno
-        return $result;
-    }
-
-    /**
-     * Método tratamentoCampos
-     *
-     * Método responsável por tratar os campos de chave estrangeira
-     * do array passado por parâmentro, onde se não houver valor vinculado
-     * ao campo ele irá atribuir null ao mesmo. Isso é necessário, pois quando não
-     * escolhido um valor para um campo de chave estrangeira no formulário de cadastro
-     * ele vem com uma string vazia "" como valor, e isso ocasiona errors sql.
-     *
-     * @param array $data
-     * @return array
-     */
-    public function tratamentoCampos(array &$data)
-    {
-        # Tratamento de campos de chaves estrangeira
-        foreach ($data as $key => $value) {
-            if(is_array($value)) {
-                foreach ($value as $key2 => $value2) {
-                    $explodeKey2 = explode("_", $key2);
-
-                    if ($explodeKey2[count($explodeKey2) -1] == "id" && $value2 == null ) {
-                        $data[$key][$key2] = null;
-                    }
-                }
-            }
-
-            $explodeKey = explode("_", $key);
-
-            if ($explodeKey[count($explodeKey) -1] == "id" && $value == null ) {
-                $data[$key] = null;
-            }
-        }
-
-        #Retorno
-        return $data;
-    }
-
-    /**
-     * Método tratamentoImagem
-     *
-     * Método que faz o tratamento das imagens enviadas para o cadastro
-     * do vestibulando.
-     *
-     * @param array $data
-     * @return array
-     */
-    public function tratamentoImagem(array &$data, $vestibulando = "")
-    {
-        #tratando a imagem
-        foreach ($data as $key => $value) {
-            $explode = explode("_", $key);
-            
-            if (count($explode) > 0 && $explode[0] == "path") {
-                $file = $data[$key];
-                $fileName = md5(uniqid(rand(), true)) . "." . $file->getClientOriginalExtension();
-
-                # Validando a atualização
-                if (!empty($vestibulando) && $vestibulando->{$key} != null) {
-                    unlink(__DIR__ . "/../../../public/" . $this->destinationPath . $vestibulando->{$key});
-                }
-
-                #Movendo a imagem
-                $file->move($this->destinationPath, $fileName);
-
-                #renomeando
-                $data[$key] = $fileName;
-            }
-        }
-
-        # retorno
-        return $data;
-    }
-
-    /**
      * [RFV003-RN004] - Documento de Requisitos
      *
      * Método responsável por gerar o número de inscrição do vestibulando
@@ -670,18 +535,18 @@ class VestibulandoService
 
         # Recuperando as matérias
         $idVestibular = $vestibulando->vestibular->id;
-        $materias     = \DB::table('fac_materias')
-                        ->select('id')
-                        ->whereIn('id', function ($query) use ($idVestibular) {
-                            $query->from('fac_vestibular_curso_materia')
-                                ->distinct()
-                                ->select('fac_vestibular_curso_materia.materia_id')
-                                ->join('fac_vestibulares_cursos', 'fac_vestibulares_cursos.id', '=', 'fac_vestibular_curso_materia.vestibular_curso_id')
-                                ->join('fac_cursos', 'fac_cursos.id', '=', 'fac_vestibulares_cursos.curso_id')
-                                ->join('fac_vestibulares', 'fac_vestibulares.id', '=', 'fac_vestibulares_cursos.vestibular_id')
-                                ->where('fac_vestibulares.id', $idVestibular)
-                                ->get();
-                        })->get();
+        $materias = \DB::table('fac_materias')
+            ->select('id')
+            ->whereIn('id', function ($query) use ($idVestibular) {
+                $query->from('fac_vestibular_curso_materia')
+                    ->distinct()
+                    ->select('fac_vestibular_curso_materia.materia_id')
+                    ->join('fac_vestibulares_cursos', 'fac_vestibulares_cursos.id', '=', 'fac_vestibular_curso_materia.vestibular_curso_id')
+                    ->join('fac_cursos', 'fac_cursos.id', '=', 'fac_vestibulares_cursos.curso_id')
+                    ->join('fac_vestibulares', 'fac_vestibulares.id', '=', 'fac_vestibulares_cursos.vestibular_id')
+                    ->where('fac_vestibulares.id', $idVestibular)
+                    ->get();
+            })->get();
 
         # Criando as notas dos alunos
         foreach ($materias as $materia) {
@@ -705,24 +570,21 @@ class VestibulandoService
      */
     public function tratamentoDebitoInscricao(Vestibulando $vestibulando)
     {
-        # Data atual
         $now = new \DateTime('now');
-
-        # Recuperando o vestibular e a taxa do vestibular
-        $vestibular     = $vestibulando->vestibular;
+        $vestibular = $vestibulando->vestibular;
         $taxaVestibular = $vestibular->taxa;
+        $contaBancaria = $this->contaBancariaRepository->getContaBancariaPadrao();
 
-        # criando o array do financeiro
-        $dados['vestibulando_id'] = $vestibulando->id;
-        $dados['vencimento']      = $now->format('d/m/Y');
-        $dados['mes_referencia']  = $now->format('m');
-        $dados['ano_referencia']  = $now->format('Y');
-        $dados['taxa_id']         = $taxaVestibular->id;
+        $dados['data_vencimento'] = $now->format('d/m/Y');
+        $dados['mes_referencia'] = $now->format('m');
+        $dados['ano_referencia'] = $now->format('Y');
+        $dados['taxa_id'] = $taxaVestibular->id;
+        $dados['valor_debito'] = $taxaVestibular->valor;
+        $dados['conta_bancaria_id'] = $contaBancaria->id;
+        $dados['pago'] = 0;
 
-        # Criação do do débito do vestibulando
-        $this->financeiroRepository->create($dados);
+        $this->debitoService->store($vestibulando, $dados);
 
-        # retorno
         return true;
     }
 
@@ -991,110 +853,6 @@ class VestibulandoService
 
         # Retorno
         return $vestibulando;
-    }
-
-    /**
-     * Método storeDebitosAbertos
-     *
-     * Método responsável por tratar os dados recebidos pelo array
-     * passado por parâmetro, e persistir os dados no banco de dados.
-     *
-     * @param array $dados
-     * @return bool
-     */
-    public function storeDebitosAbertos(array $dados)
-    {
-        # Regras de negócios
-        $this->tratamentoCampos($dados);
-
-        # Cadastrando
-        $vestibulandoFinanceiro = $this->financeiroRepository->create($dados);
-
-        # Regras de negócios
-        $this->tratamentoInscricao($vestibulandoFinanceiro, $vestibulandoFinanceiro->vestibulando);
-
-        # Retorno
-        return true;
-    }
-
-    /**
-     * Método updateDebitosAbertos
-     *
-     * Método responsável por tratar os dados recebidos pelo array
-     * passado por parâmetro, e persistir os dados no banco de dados.
-     *
-     * @param array $dados
-     * @return bool
-     */
-    public function updateDebitosAbertos(array $dados, int $id)
-    {
-        # Regras de negócios
-        $this->tratamentoCampos($dados);
-
-        # Atualizando
-        $vestibulandoFinanceiro = $this->financeiroRepository->update($dados, $id);
-
-        # Regras de negócios
-        $this->tratamentoInscricao($vestibulandoFinanceiro, $vestibulandoFinanceiro->vestibulando);
-
-        # Retorno
-        return true;
-    }
-
-    /**
-     * Método findDebito
-     *
-     * Método responsável por recupear uma instância específica(id)
-     * do débito com todas as suas dependências.
-     *
-     * @param int $id
-     * @return mixed
-     * @throws \Exception
-     */
-    public function findDebito(int $id)
-    {
-        #Recuperando o registro no banco de dados
-        $relacionamentos = [
-            'taxa',
-            'taxa.tipoTaxa'
-        ];
-
-        $debito = $this->financeiroRepository->with($relacionamentos)->find($id);
-
-        #Verificando se o registro foi encontrado
-        if(!$debito) {
-            throw new \Exception('Vestibulando não encontrado!');
-        }
-
-        #retorno
-        return $debito;
-    }
-
-    /**
-     * Método deleteDebitosAbertos
-     *
-     * Método responsável por deletar um débito específico(id)
-     * do banco de dados.
-     *
-     * @param int $id
-     * @return bool
-     * @throws \Exception
-     */
-    public function deleteDebitosAbertos(int $id)
-    {
-        # Recuperando o débito
-        $debito = $this->financeiroRepository->find($id);
-
-        # Verificando se existe
-        if(!$debito) {
-             throw new \Exception('Débito não encontrado');
-         }
-
-        # Transação de remoção
-        $this->financeiroRepository->delete($id);
-
-        # retorno
-        return true;
     }
 
     /**
