6c25a5e3a353b014f619d78cdf3975dccf736248
andreypriscila <andreyfbs2013@gmail.com>
2017-10-20 13:55:43 -0300

    Atualizações

--- app/Services/PosGraduacao/ProfessorPosService.php
+++ app/Services/PosGraduacao/ProfessorPosService.php
@@ -7,9 +7,12 @@ use Seracademico\Entities\PosGraduacao\ProfessorPos;
 use Seracademico\Repositories\EnderecoRepository;
 use Seracademico\Repositories\PessoaRepository;
 use Carbon\Carbon;
+use Seracademico\Services\TraitService;
 
 class ProfessorPosService
 {
+    use TraitService;
+    
     /**
      * @var ProfessorPosRepository
      */
@@ -227,149 +230,6 @@ class ProfessorPosService
 
         #Retorno
         return $professor;
-    }
-
-    /**
-     * @param $id
-     */
-    public function insertImg($id, $tipo)
-    {
-        #tratando a imagem
-        if(isset($_FILES['img']['tmp_name']) && $_FILES['img']['tmp_name'] != null) {
-
-            $tmpName = $_FILES['img']['tmp_name'];
-
-            $fp = fopen($tmpName, 'r');
-
-            $add = fread($fp, filesize($tmpName));
-
-            $add = addslashes($add);
-
-            fclose($fp);
-
-            $pdo = \DB::connection()->getPdo();
-
-            $query = "UPDATE fac_professores SET path_image = '{$add}', tipo_img = {$tipo} where id =  $id ";
-
-            $pdo->query($query);
-
-        }
-
-    }
-
-    /**
-     * @param array $data
-     * @return array
-     */
-    public function tratamentoImagem(array &$data, $model = "")
-    {
-        #tratando a imagem
-        foreach ($data as $key => $value) {
-            $explode = explode("_", $key);
-
-            if (count($explode) > 0 && $explode[0] == "path") {
-                $file = $data[$key];
-                $fileName = md5(uniqid(rand(), true)) . "." . $file->getClientOriginalExtension();
-
-                # Validando a atualização
-                if (!empty($model) && $model->{$key} != null) {
-                    unlink(__DIR__ . "/../../../public/" . $this->destinationPath . $model->{$key});
-                }
-
-                #Movendo a imagem
-                $file->move($this->destinationPath, $fileName);
-
-                #renomeando
-                $data[$key] = $fileName;
-
-            }
-        }
-
-        # retorno
-        return $data;
-    }
-
-    /**
-     * @param array $models
-     * @param bool $ajax
-     * @return array
-     */
-    public function load(array $models, $ajax = false) : array
-    {
-        #Declarando variáveis de uso
-        $result    = [];
-        $expressao = [];
-
-        #Criando e executando as consultas
-        foreach ($models as $model) {
-            # separando as strings
-            $explode   = explode("|", $model);
-
-            # verificando a condição
-            if(count($explode) > 1) {
-                $model     = $explode[0];
-                $expressao = explode(",", $explode[1]);
-            }
-
-            #qualificando o namespace
-            $nameModel = "\\Seracademico\\Entities\\$model";
-
-            #Verificando se existe sobrescrita do nome do model
-            $model     = isset($expressao[2]) ? $expressao[2] : $model;
-
-            if ($ajax) {
-                if(count($expressao) > 1) {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::{$expressao[0]}($expressao[1])->orderBy('nome', 'asc')->get(['nome', 'id']);
-                } else {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::orderBy('nome', 'asc')->get(['nome', 'id']);
-                }
-            } else {
-                if(count($expressao) > 1) {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::{$expressao[0]}($expressao[1])->lists('nome', 'id');
-                } else {
-                    #Recuperando o registro e armazenando no array
-                    $result[strtolower($model)] = $nameModel::lists('nome', 'id');
-                }
-            }
-
-            # Limpando a expressão
-            $expressao = [];
-        }
-
-        #retorno
-        return $result;
-    }
-
-    /**
-     * @param array $data
-     * @return array
-     */
-    public function tratamentoCampos(array &$data)
-    {
-        # Tratamento de campos de chaves estrangeira
-        foreach ($data as $key => $value) {
-            if(is_array($value)) {
-                foreach ($value as $key2 => $value2) {
-                    $explodeKey2 = explode("_", $key2);
-
-                    if ($explodeKey2[count($explodeKey2) -1] == "id" && $value2 == null ) {
-                        $data[$key][$key2] = null;
-                    }
-                }
-            }
-
-            $explodeKey = explode("_", $key);
-
-            if ($explodeKey[count($explodeKey) -1] == "id" && $value == null ) {
-                $data[$key] = null;
-            }
-        }
-
-        #Retorno
-        return $data;
     }
 
     /**
