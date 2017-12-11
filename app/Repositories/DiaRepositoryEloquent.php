5b6dab80ac328eff5144a638b5894e821e9ede72
Felipe <felipeldnascimento@gmail.com>
2017-12-11 11:14:32 -0300

    atualizações

--- app/Repositories/DiaRepositoryEloquent.php
+++ app/Repositories/DiaRepositoryEloquent.php
@@ -24,8 +24,6 @@ class DiaRepositoryEloquent extends BaseRepository implements DiaRepository
         return Dia::class;
     }
 
-    
-
     /**
      * Boot up the repository, pushing criteria
      */
