cecdad0fb172583123c6175716d0371001b6acc5
andreypriscila <andreyfbs2013@gmail.com>
2018-01-24 07:12:18 -0300

    Testes

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+
     <title>SerAcadêmico - Gestão Acadêmica</title>
 
     <link href="{{ asset('/css/bootstrap.min.css')}}" rel="stylesheet">
