f2368921808cc5dd7ce39a4ca1530439cad9dc06
Gustavo Dantas <dz6.gustav@gmail.com>
2018-04-03 10:21:47 -0300

    novo modelo viewPosProfessorVinculo

--- resources/views/reports/simple/viewPosProfessorVinculo.blade.php
+++ resources/views/reports/simple/viewPosProfessorVinculo.blade.php
@@ -1,26 +1,45 @@
 <html>
 <head>
+    {{--Documento personalizado em 03/04/2018 @Gustavo--}}
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <title>Declaração de vínculo</title>
+    <style>
+    .borda{
+       height: 915px;
+       border: 5px solid black;
+       padding: 10px 50px;
+       display: block; 
+       margin: 0;   
+   }
+</style>
 </head>
 
 <body>
-    <div class="cabecalho">
+    <div class="borda">
+      <div class="cabecalho">
+
         <h1>
-            <img width="185" src="{{ asset('img/dd.jpg') }}" alt="" style="position: relative; left: 500px; top: 50px;">
+            <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; left: 500px;">
         </h1>
+        <h1>
+            <center>  <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; width: 500px;">
+            </center>
+        </h1>
+
+
     </div>
 
     <div class="conteudo">
         <h2 style="text-align: center; margin-top: 30px;">
-            DECLARAÇÃO
+            <b>DECLARAÇÃO</b>
         </h2>
-        <p style="font-size: 18px; text-align: justify; line-height: 50px; text-indent: 30px;">
-            Declaramos para fins de comprovação que o(a) Professor(a)
-            <strong>{{ $dados['body'][count($dados['body'])-1]->professor }}</strong>, Portadora do CPF:
+        <p style="font-size: 18px; text-align: justify;">
+            Declaramos para fins de comprovação que o (a) Professor (a)
+            Convidado (a)
+            <strong>{{ $dados['body'][count($dados['body'])-1]->professor }}</strong>, CPF:
             <strong>{{ $dados['body'][count($dados['body'])-1]->cpf }}</strong> ministrou em nossa instituição a Disciplina
-            <strong>{{ $dados['body'][count($dados['body'])-1]->disciplina }}</strong>, no Curso de Especialização, Pós-Graduação (LATO SENSU) em
+            <strong>{{ $dados['body'][count($dados['body'])-1]->disciplina }}</strong>, no Curso de Pós-Graduação (LATO SENSU) em
             <strong>{{ $dados['body'][count($dados['body'])-1]->curso }}</strong>, com carga horária de
             <strong>{{ $dados['body'][count($dados['body'])-1]->carga_horaria }}</strong> horas, em {{ strftime('%B de %Y', strtotime('today')) }}.
         </p>
@@ -28,37 +47,41 @@
             <div>
                 O referido é verdadeiro e dou fé.
             </div>
-            <p style="margin-top: 20px;">
-                <strong>Recife, {{ strftime('%d de %B de %Y', strtotime('today')) }}</strong>
+            <p style="position: relative; left: 200px; top:50px">
+                <strong>Recife, {{ strftime('%d de %B de %Y', strtotime('today')) }}.</strong>
             </p>
+            <p style=" position:relative; left:150px; top:100px; display:block;width: 300px;font-size: 14px;color: #13205D" ><b>&nbsp;&nbsp;&nbsp; Faculdade Alpha</b> – CNPJ: 15.708.483/0001-50
+                Rua Gervásio Pires, nº 826, Santo Amaro – Recife – PE.
+            Fones: (81) 3071-7249 / 99516-2229/ 98446-0808</p>
         </div>
     </div>
+</div>
+<div class="borda">
+  <div class="cabecalho">
 
-    <div class="assinatura" style="font-size: 18px">
-        <p>
-            <h1><img  src="{{ asset('img/assinatura_luciana.png') }}" alt="" style="margin-left: 20%; width: 330px; height: auto;"></h1>
-        </p>
-        <p style="position: absolute; top: 80px; margin-left: 27%;">
-            ___________________________________
-        </p>
-        <p style="position: absolute; top: 80px; margin-left: 45%;">
-            Direção
-        </p>
-    </div>
+    <h1>
+        <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; left: 500px;">
+    </h1>
+    <h1>
+        <center>  <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; width: 500px;">
+        </center>
+    </h1>
+</div>
 
-    <div class="rodape" style="text-align: center; font-size: 18px; margin-top: 10px">
-        <div>
-            ALPHA EDUCAÇÃO E TREINAMENTOS – CNPJ: 22.945.385/0001-00
-        </div>
-        <div>
-            Rua Gervásio Pires, nº 826, Santo Amaro – Recife – PE.
-        </div>
-        <div>
-            Facebook.com/faculdadealpha
-        </div>
-        <div>
-            Fones: (81) 3071-7249 / 99516-2229 / 98446-0808
-        </div>
-    </div>
-</body>
+
+<div class="assinatura" style="font-size: 18px; position: relative; top:100px; right:80px;">
+    <p>
+        <h1><img  src="{{ asset('img/assinatura_luciana.png') }}" alt="" style="margin-left: 20%; width: 330px; height: auto;"></h1>
+    </p>
+    <p style="position: absolute; top: 100px; margin-left: 27%;padding-left: 80px;">
+        Luciana Teixeira Vitor
+    </p>
+    <p style="position: absolute; top: 120px; margin-left: 45%;">
+        Direção Geral
+    </p>
+</div>
+
+<p style=" position:relative; left:150px;bottom: 0px; top:200px; display:block;width: 300px;font-size: 14px;color: #13205D" ><b>&nbsp;&nbsp;&nbsp; Faculdade Alpha</b> – CNPJ: 15.708.483/0001-50
+    Rua Gervásio Pires, nº 826, Santo Amaro – Recife – PE.
+Fones: (81) 3071-7249 / 99516-2229/ 98446-0808</p></body>
 </html>
