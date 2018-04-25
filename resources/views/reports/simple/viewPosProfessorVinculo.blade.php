c65ac7f409a351e7020a62800384640ae81894da
Pancosta <dz6.gustav@gmail.com>
2018-04-25 09:47:57 -0300

    novo design Declaração de vinculo professor Pós

--- resources/views/reports/simple/viewPosProfessorVinculo.blade.php
+++ resources/views/reports/simple/viewPosProfessorVinculo.blade.php
@@ -1,37 +1,43 @@
 <html>
 <head>
     {{--Documento personalizado em 03/04/2018 @Gustavo--}}
+    {{--Documento personalizado em 25/04/2018 @Gustavo--}}
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <title>Declaração de vínculo</title>
     <style>
+
     .borda{
        height: 915px;
        border: 5px solid black;
        padding: 10px 50px;
        display: block; 
-       margin: 0;   
+       margin: 0;  
    }
+    .background{
+      width: 70%;
+      z-index: -10; 
+      opacity: 0.3;
+      position: absolute;
+      top: 350px;
+      left: 150px;
+    } 
     </style>
 </head>
 
 <body>
     <div class="borda">
+        <img class="background" src="{{ asset('img/logo-alpha.png') }}" alt="">
       <div class="cabecalho">
-
+       
         <h1>
             <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; left: 500px;">
         </h1>
-        <h1>
-            <center>  <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; width: 500px;">
-            </center>
-        </h1>
-
 
     </div>
 
     <div class="conteudo">
-        <h2 style="text-align: center; margin-top: 30px;">
+        <h2 style="text-align: center; margin-top: 100px;">
             <b>DECLARAÇÃO</b>
         </h2>
         <p style="font-size: 18px; text-align: justify;">
@@ -50,28 +56,9 @@
             <p style="position: relative; left: 200px; top:50px">
                 <strong>Recife, {{ strftime('%d de %B de %Y', strtotime('today')) }}.</strong>
             </p>
-            <p style=" position:relative; left:150px; top:100px; display:block;width: 300px;font-size: 14px;color: #13205D" ><b>&nbsp;&nbsp;&nbsp; Faculdade Alpha</b> – CNPJ: 15.708.483/0001-50
-                Rua Gervásio Pires, nº 826, Santo Amaro – Recife – PE.
-            Fones: (81) 3071-7249 / 99516-2229/ 98446-0808</p>
-        </div>
-    </div>
-</div>
-<div class="borda">
-  <div class="cabecalho">
-
-    <h1>
-        <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; left: 500px;">
-    </h1>
-    <h1>
-        <center>  <img width="130" src="{{ asset('img/logo-alpha_640x344.png') }}" alt="" style="position: relative; width: 500px;">
-        </center>
-    </h1>
-</div>
-
-
-<div class="assinatura" style="font-size: 18px; position: relative; top:100px; right:80px;">
+            <div class="assinatura" style="font-size: 18px; position: relative; top:100px; right:80px;">
     <p>
-        <h1><img  src="{{ asset('img/assinatura_luciana.png') }}" alt="" style="margin-left: 20%; width: 330px; height: auto;"></h1>
+        <h1><img  src="{{ asset('img/assinatura_luciana.png') }}" alt="" style="margin-right: 10%; width: 330px; height: auto;"></h1>
     </p>
     <p style="position: absolute; top: 100px; margin-left: 27%;padding-left: 80px;">
         Luciana Teixeira Vitor
@@ -80,8 +67,10 @@
         Direção Geral
     </p>
 </div>
-
-<p style=" position:relative; left:150px;bottom: 0px; top:200px; display:block;width: 300px;font-size: 14px;color: #13205D" ><b>&nbsp;&nbsp;&nbsp; Faculdade Alpha</b> – CNPJ: 15.708.483/0001-50
-    Rua Gervásio Pires, nº 826, Santo Amaro – Recife – PE.
-Fones: (81) 3071-7249 / 99516-2229/ 98446-0808</p></body>
+            <p style=" position:relative; left:150px; top:100px; display:block;width: 300px;font-size: 14px;color: #13205D" ><b>&nbsp;&nbsp;&nbsp; Faculdade Alpha</b> – CNPJ: 15.708.483/0001-50
+                Rua Gervásio Pires, nº 826, Santo Amaro – Recife – PE.
+            Fones: (81) 3071-7249 / 99516-2229/ 98446-0808</p>
+        </div>
+    </div>
+</div>
 </html>
